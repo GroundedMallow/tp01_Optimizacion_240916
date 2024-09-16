01534b8f62bf2e6a8a7ccf4c36d2292fe598c060
GroundedMallow <alexisberrios.a@gmail.com>
2024-09-16 17:19:27 -0300

    test3

--- Assets/Scripts/EventSpawn.cs
+++ Assets/Scripts/EventSpawn.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EventSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] UnityEvent onTriggerEnter;
+    [SerializeField] UnityEvent onTriggerExit;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        onTriggerEnter.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        onTriggerExit.Invoke();
     }
 }
